dcda79fc793fa4437f012f78252301dc942375e8
Italo Cruz <italocruz544@gmail.com>
2024-10-02 16:48:30 -0300

    create project4 (bootstrap in the CI)

---
+++ projeto4/application/views/layout/cabecalho.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+    <title>Document</title>
+</head>
+<body>
+
+    <h3>Cabeçalho</h3>
+
+    <hr>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="<?php echo site_url('geral')?>">Página 01</a>
+                <a href="<?php echo site_url('geral/pag02')?>">página 02</a>
+                <a href="<?php echo site_url('geral/pag03')?>">página 03</a>
+            </li>
+        </ul>
+    </nav>
