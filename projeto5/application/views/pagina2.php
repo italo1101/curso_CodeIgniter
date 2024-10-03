c23987aa68f1e3c5571929c14ec15c79bbb768ed
Italo Cruz <italocruz544@gmail.com>
2024-10-03 13:42:04 -0300

    add project5 (work with controllers data)

---
+++ projeto5/application/views/pagina2.php
@@ -0,0 +1,7 @@
+<h4>
+    Página 02
+
+    <img src="<?php echo base_url('assets/images/wallpaper.jpg ') ?>" alt="bridge_wallpaper">
+</h4>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptas perspiciatis. Ex quam alias, quasi reiciendis laudantium, sequi animi tempore velit delectus fuga possimus beatae illo fugiat, dicta provident in.</p>
